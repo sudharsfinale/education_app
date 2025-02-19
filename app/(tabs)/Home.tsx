ec80876924ed788a11fd14cec2b7c6311adba1d3
sudharsfinale <sudharsfinale@gmail.com>
2025-02-19 23:56:58 +0530

    Add no course component

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,11 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
 import Header from "@/components/Home/Header";
+import NoCoursePage from "@/components/Home/NoCoursePage";
 
 const Home = () => {
   return (
     <View style={{ padding: 25 }}>
       <Header />
+      <NoCoursePage />
     </View>
   );
 };
---
+++ components/Home/NoCoursePage.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NoCoursePage = () => {
+  return (
+    <View>
+      <Text>NoCoursePage</Text>
+    </View>
+  );
+};
+
+export default NoCoursePage;
