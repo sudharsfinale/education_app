b82e0402425d73f193dfc967850e49fdcd075d7e
sudharsfinale <sudharsfinale@gmail.com>
2025-02-26 20:49:24 +0530

    Get courses from firebase db using email id and show No course only if got no courses

--- app/(tabs)/Home.tsx
+++ app/(tabs)/Home.tsx
@@ -1,14 +1,39 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Header from "@/components/Home/Header";
 import NoCoursePage from "@/components/Home/NoCoursePage";
 import { colors } from "@/constants/Colors";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/config/firebaseConfig";
+import CourseList from "@/components/Home/CourseList";
 
 const Home = () => {
+  //@ts-ignore
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+  const [courseList, setCourseList] = useState<any>([]);
+  const getCourseList = async () => {
+    const q = query(
+      collection(db, "Courses"),
+      where("createdBy", "==", userDetail?.email)
+    );
+    const querySnapShot = await getDocs(q);
+    querySnapShot?.forEach((doc) => {
+      setCourseList((prev: any) => [...prev, doc.data()]);
+    });
+  };
+  useEffect(() => {
+    getCourseList();
+  }, []);
+  console.log(courseList);
   return (
     <View style={{ padding: 25, backgroundColor: colors.WHITE, flex: 1 }}>
       <Header />
-      <NoCoursePage />
+      {Array.isArray(courseList) && courseList?.length ? (
+        <CourseList />
+      ) : (
+        <NoCoursePage />
+      )}
     </View>
   );
 };
---
+++ components/Home/CourseList.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CourseList = () => {
+  return (
+    <View>
+      <Text>CourseList</Text>
+    </View>
+  );
+};
+
+export default CourseList;
