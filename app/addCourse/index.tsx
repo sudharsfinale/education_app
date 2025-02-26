8247dafe480564037afe7be085df238f33097690
sudharsfinale <sudharsfinale@gmail.com>
2025-02-26 07:41:06 +0530

    Generate and save course and takes to home after saving

--- app/addCourse/index.tsx
+++ app/addCourse/index.tsx
@@ -1,15 +1,25 @@
 import { View, Text, TextInput, ScrollView, Pressable } from "react-native";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { colors } from "@/constants/Colors";
 import Button from "@/components/Shared/Button";
 import { GenerateCourseAiModel, GenerateTopicsAiModel } from "@/config/AiModel";
 import Prompt from "@/constants/Prompt";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/config/firebaseConfig";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { useRouter } from "expo-router";
 
 const AddCourse = () => {
   const [loading, setLoading] = useState(false);
   const [userInput, setUserInput] = useState("");
   const [topics, setTopics] = useState([]);
   const [selectedTopics, setSelectedTopics] = useState<any>([]);
+
+  //@ts-ignore
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
+
+  const router = useRouter();
+
   const onGenerateTopic = async () => {
     try {
       setLoading(true);
@@ -26,19 +36,28 @@ const AddCourse = () => {
     }
   };
   const onGenerateCourse = async () => {
+    setLoading(true);
+    const PROMPT = selectedTopics + Prompt.COURSE;
     try {
-      setLoading(true);
-      const PROMPT = selectedTopics + Prompt.COURSE;
       const airesponse = await GenerateCourseAiModel.sendMessage(PROMPT);
-      let course = JSON.parse(airesponse.response.text());
-      console.log(course);
+      let courses = JSON.parse(airesponse.response.text());
+      courses?.forEach(async (course: any) => {
+        await setDoc(doc(db, "Courses", Date.now().toString()), {
+          ...course,
+          createdOn: new Date(),
+          createdBy: userDetail?.email,
+        });
+      });
+      console.log(courses);
+      setLoading(false);
+      router.push("/(tabs)/Home");
     } catch (error) {
       console.log(error);
+      setLoading(false);
     } finally {
       setLoading(false);
     }
   };
-  console.log("hi");
   const onTopicSelect = (topic: string) => {
     let isTopicAlreadySelected = selectedTopics.find(
       (_topic: any) => _topic === topic
