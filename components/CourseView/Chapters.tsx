8a9366aa9bb47ffd3d07cc5f5ad6b0269a10116e
sudharsfinale <sudharsfinale@gmail.com>
2025-03-10 09:44:10 +0530

    Add courseId in params to refetch after updating, also finish chapter view page, connect onChaptercomplete call

--- components/CourseView/Chapters.tsx
+++ components/CourseView/Chapters.tsx
@@ -1,26 +1,69 @@
 import { View, Text, FlatList, Pressable, StyleSheet } from "react-native";
 import React from "react";
 import Entypo from "@expo/vector-icons/Entypo";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { colors } from "@/constants/Colors";
+import { useRouter } from "expo-router";
 
 const Chapters = ({ course }: any) => {
   const course_chapters = course?.chapters || [];
 
+  const router = useRouter();
   const ChapterItem = ({
     item,
     chapterIndex,
   }: {
     item: any;
     chapterIndex: number;
-  }) => (
-    <Pressable style={styles.chapterItem}>
-      <View style={styles.chapterRow}>
-        <Text style={styles.chapterIndex}>{chapterIndex + 1}. </Text>
-        <Text style={styles.chapterText}>{item?.chapterName}</Text>
-      </View>
-      <Entypo name="controller-play" size={24} color={colors.PRIMARY} />
-    </Pressable>
-  );
+  }) => {
+    const isChapterCompleted = () => {
+      let completed_chapters = course?.completedChapters;
+      if (
+        completed_chapters &&
+        Array.isArray(completed_chapters) &&
+        completed_chapters
+      ) {
+        let chapter_index = course?.completedChapters.find(
+          (_index: any) => Number(_index) === chapterIndex
+        );
+        if (chapter_index) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    };
+    let chapter_completed = isChapterCompleted();
+    return (
+      <Pressable
+        onPress={() =>
+          router.push({
+            pathname: "/chapterView",
+            params: {
+              chapterParams: JSON.stringify(item),
+              docId: course?.docId,
+              chapterIndex,
+            },
+          })
+        }
+        style={styles.chapterItem}
+      >
+        <View style={styles.chapterRow}>
+          <Text style={styles.chapterIndex}>{chapterIndex + 1}. </Text>
+          <Text style={styles.chapterText}>{item?.chapterName}</Text>
+        </View>
+        <View style={{ flex: 0.1, alignItems: "center" }}>
+          {chapter_completed ? (
+            <AntDesign name="checkcircle" size={24} color={colors.GREEN} />
+          ) : (
+            <Entypo name="controller-play" size={24} color={colors.PRIMARY} />
+          )}
+        </View>
+      </Pressable>
+    );
+  };
 
   return (
     <View style={styles.container}>
@@ -64,6 +107,7 @@ const styles = StyleSheet.create({
   chapterRow: {
     flexDirection: "row",
     gap: 10,
+    flex: 0.9,
   },
   chapterIndex: {
     fontFamily: "outfit-bold",
@@ -72,6 +116,9 @@ const styles = StyleSheet.create({
   chapterText: {
     fontFamily: "outfit-bold",
     fontSize: 18,
+    alignSelf: "flex-start", // Ensures text moves to the next line when needed
+    maxWidth: "100%", // Prevents text from exceeding the parent width
+    flexShrink: 1,
   },
 });
 
