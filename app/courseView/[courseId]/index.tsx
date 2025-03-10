ab02d7b7065d493f3805e149f0186fad54307774
sudharsfinale <sudharsfinale@gmail.com>
2025-03-10 23:57:26 +0530

    Add loader on course Fetch

--- app/courseView/[courseId]/index.tsx
+++ app/courseView/[courseId]/index.tsx
@@ -1,4 +1,11 @@
-import { View, Text, Image, ScrollView, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ScrollView,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 import { imageAssets } from "@/constants/Options";
@@ -12,22 +19,25 @@ import { db } from "@/config/firebaseConfig";
 const CourseView = () => {
   const { courseParams, courseId }: any = useLocalSearchParams();
   const [courseData, setCourseData] = useState(null);
+  const [gettingCourseData, setGettingCourseData] = useState(false);
   const getCourseById = async () => {
     const docRef = await getDoc(doc(db, "Courses", courseId));
     let course_data: any = docRef.data();
     if (course_data) {
       setCourseData(course_data);
+      setGettingCourseData(false);
     }
   };
   useEffect(() => {
     if (!courseParams && courseId) {
+      setGettingCourseData(true);
       getCourseById();
     } else if (courseParams) {
       const course: any = JSON.parse(courseParams);
       setCourseData(course);
     }
   }, [courseId]);
-  return (
+  return courseData ? (
     <FlatList
       data={[]}
       renderItem={() => <View></View>}
@@ -39,7 +49,18 @@ const CourseView = () => {
         </View>
       }
     ></FlatList>
-  );
+  ) : gettingCourseData ? (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.WHITE,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ActivityIndicator color={colors.PRIMARY} size={50} />
+    </View>
+  ) : null;
 };
 
 export default CourseView;
